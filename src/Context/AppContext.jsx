97c0e6460ca5051a4f933ca8863a63b7a96135b6
Ramvilasteku <ramvilasteku@gmail.com>
2025-05-17 16:51:58 +0530

    new

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { humanizerDuration } from "humanize-duration";
+import { TbOmega } from "react-icons/tb";
 
 export const AppContext = createContext();
 
@@ -32,6 +34,43 @@ export const AppContextProvider = (props) => {
       return totalRating / course.courseRatings.length
   }
 
+
+
+  //  Function to calculate course chapter time
+
+  const calculateChapterTime = (chapter)=>{
+    let time = 0
+    chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration)
+   return humanizerDuration(time * 60 * 1000,{units: ['h', 'm']}) 
+  
+  }
+
+
+  // Function to calculate course duration
+
+  const calculateCourseDuration = (course) => {
+    let time = 0
+    course,courseContent.map((chapter)=> chapter.chapterContent.map(
+      (lecture) => time += lecture.lectureDuration
+    ))
+   return humanizerDuration(time * 60 * 1000,{units: ['h', 'm']}) 
+
+  }
+
+    // Function to calculate no of lectures in the course
+
+    const calculateNoOfLectures = (course)=> {
+
+      let totalLectures = 0;
+      course.courseContent.forEach(chapter => {
+        if(Array.isArray(chapter.chapterContent)){
+          totalLectures += chapter.chapterContent.length
+      }
+    });
+    return totalLectures;
+    }
+
+
   useEffect(() => {
     fetchAllCourses();
   }, []);
@@ -39,9 +78,12 @@ export const AppContextProvider = (props) => {
   const value = {
     currency,
     allCourses,
+    isEducator,setIsEducator,
     navigate,
     calculateRating,
-    isEducator,setIsEducator
+    calculateNoOfLectures,
+    calculateCourseDuration,
+    calculateChapterTime
   };
 
   return (
--- src/Pages/PgsStudent/CourseDetails.jsx
+++ src/Pages/PgsStudent/CourseDetails.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../Context/AppContext";
 import Loading from "../../Components/Student/Loading";
+import { assets } from "../../assets/assets";
 
 const CourseDetails = () => {
   const { id } = useParams();
@@ -10,7 +11,7 @@ const CourseDetails = () => {
 
   // console.log(setCourseData);
 
-  const { allCourses } = useContext(AppContext);
+  const { allCourses, calculateRating, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime } = useContext(AppContext);
 
   const fetchCourseData = async () => {
     const findCourse = await allCourses.find((course) => course._id === id);
@@ -26,19 +27,61 @@ const CourseDetails = () => {
       <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
         <div className="absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-gray-400/90"></div>
         {/* Left Col */}
-        <div>
-          <h1>{courseData.courseTitle} </h1>
-          <p dangerouslySetInnerHTML={{__html : courseData.courseDescription.slice(0,200)} }></p>
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="md:text-[36px] text-[24px] font-semibold text-gray-800">
+            {courseData.courseTitle}{" "}
+          </h1>
+          <p
+            className="pt-4 md:text-base  text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+
+          {/* Rating */}
+          <div className="flex items-center space-x-2">
+            <p>{calculateRating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt="rating-icon"
+                  className="w-3.5 h-3.5"
+                />
+              ))}
+            </div>
+            <p className="text-gray-600">
+              ({courseData.courseRatings.length}{" "}
+              {courseData.courseRatings.length > 1 ? "Ratings" : "Rating"})
+            </p>
+
+            <p>
+              {courseData.enrolledStudents.length}{" "}
+              {courseData.enrolledStudents.length > 1 ? "Students" : "Student"}
+            </p>
+          </div>
+
+          <p className="text-lg">Course by <span className="text-orange-500 underline">Ajay Vemula</span></p>
+
+          <div className="pt-8 text-gray-800">
+            <h2 className="text-xl font-semibold">Course Structure</h2>
+
+          </div>
         </div>
 
         {/* RightCol */}
 
         <div></div>
-
       </div>
     </>
-  ) :   <Loading />
-  
+  ) : (
+    <Loading />
+  );
 };
 
 export default CourseDetails;
